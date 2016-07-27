62826a8c75022d14f75c705f0bd6d3a4c6374029
Amannor <alon.mannor@gmail.com>
2016-07-27 21:57:37 +0300

    Added 1st stage of meeting test - haven't finished the test function yet

--- src/communities/tests/example_live_test.py
+++ src/communities/tests/example_live_test.py
@@ -30,17 +30,27 @@ class ExampleCommunityLiveTests(StaticLiveServerTestCase):
         self.committee = Committee.objects.create(name="Culture", slug="culture", community=self.community)
 
     def full_url(self, s):
-        return self.live_server_url + s
+        if s.startswith(self.live_server_url):
+            return s
+        else:
+            return self.live_server_url + s
 
     def get_current_path(self):
         return urlparse.urlsplit(self.selenium.current_url).path
 
     def assert_current_path(self, path):
-        self.assertEqual(path, self.get_current_path())
+        self.assertEqual(self.full_url(path), self.full_url(self.get_current_path()))
+
+    def selenium_get_and_assert(self, url):
+        """
+        Tries to go url (as is) and then asserts that current_path == url
+        """
+        self.selenium.get(url)
+        self.assert_current_path(url)
 
     def login(self, goto_login):
         if goto_login is True:
-            self.selenium.get(reverse('login'))
+            self.selenium.get(self.full_url(reverse('login')))
         self.assert_current_path(reverse('login'))
         username_input = self.selenium.find_element_by_id("id_username")
         username_input.send_keys(self.u1.email)
@@ -53,32 +63,29 @@ class ExampleCommunityLiveTests(StaticLiveServerTestCase):
         self.selenium.get(url)
         self.login(False)  # False since the community we created is private - it should automatically redircet to login
         self.selenium.get(self.full_url(reverse('logout')))  # logging out at the end of every test
+
+    def test_create_meeting(self):
+        self.login(True)
+        """
+        TODO - after login goto the community page, locate the relevant html element for the committee - and 'click()' it
+        (The below code didn't work because click didn't work for me - idk y
+        url = self.full_url(self.community.get_absolute_url())
+        self.selenium_get_and_assert(url)
+
+        self.selenium.find_element_by_class_name(
+            "panel-heading").click()  # goto "Next meeting" (TODO: check if this works if we have single \ multiple meetings in the system)
+        self.assert_current_path(
+            self.committee.get_absolute_url())  # note: we're seeing a new meeting in progress - hence its' url is the committees'
+        """
+        url = self.full_url(self.committee.get_absolute_url())
+        self.selenium_get_and_assert(url)
+        # Assert that we're seeing all the communities we're a member of
+        new_subject_input = self.selenium.find_element_by_id("quick-issue-title")
+        new_subject_input.send_keys("dummy-subject1")
         # from IPython import embed
         # embed()
-        # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
-        # username_input = self.selenium.find_element_by_name("username")
-        # username_input.send_keys('myuser')
-        # password_input = self.selenium.find_element_by_name("password")
-        # password_input.send_keys('secret')
-        # self.selenium.find_elehment_by_xpath('//input[@value="Log in"]').click()
-
-        # def test_sdfsdf(self):
-        #     self.login(True)
-        #     self.selenium.get(self.full_url(self.community.get_absolute_url()))
-        #     self.assert_current_path(reverse(self.community.get_absolute_url()))
-
-    # def test_create_meeting(self):
-    #     self.login(True)
-    #     url = self.full_url(self.community.get_absolute_url())
-    #     self.selenium.get(url)
-    #     self.selenium.find_element_by_class_name(
-    #         "panel-heading").click()  # goto "Next meeting" (TODO - check if this works if we have past meetings in the system)
-    #     self.assert_current_path(self.full_url(
-    #         self.committee.get_absolute_url()))  # note: we're seeing a new meeting in progress - hence its' url is the committees'
-    #     # Assert that we're watching all the communities we're a member of
-    #     new_subject_input = self.selenium.find_element_by_id("quick-issue-title")
-    #     new_subject_input.send_keys("dummy-subject1")
-    #     self.selenium.find_element_by_xpath('//input[@type="submit"]').click()
-    #     raw_input()
+        self.selenium.find_element_by_xpath(
+            '//button[@type="submit"]').click()  # should act identical to: self.selenium.find_element_by_id("quick-issue-add").click()
+        self.selenium.get(self.full_url(reverse('logout')))  # logging out at the end of every test
 
         # Create a new committee & assert what's needed
